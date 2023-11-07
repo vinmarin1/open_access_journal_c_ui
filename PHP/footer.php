56135d34cb4c2d1f12033fdc3c6d46cb986226e1
vinmarin1 <marvin.gozo23@gmail.com>
2023-11-07 20:19:26 +0800

    home, login and register UI

---
+++ PHP/footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="../CSS/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="footer-container">
+        <div class="footer">
+          <a class="navbar-brand" href="#"><img class="img-fluid"  src="../images/qcu-logo.webp" alt="">
+          <div class="f-logotitle">
+          QUEZON CITY UNIVERSITY</a>
+          <span>OPEN ACCES JOURNAL</span>
+          </div>
+        </div>
+        
+        <div class="copyright-container">
+          <div class="copyright">Content on this site is licensed under a Creative Commons <a href="https://creativecommons.org/licenses/by-sa/4.0/">Attribution-<br>ShareAlike 4.0 International (CC BY-SA 4.0) license.</a>
+        </div>
+        </div>
+        <div class="links-container">
+          <div class="links">
+          <a href="#">Home</a>
+          <a class="footer-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            About
+          </a>
+          <ul class="dropdown-menu">
+          <li><a class="dropdown-item" style="color: black" href="#">THE WEBSITE</a></li>
+          <li><a class="dropdown-item" style="color: black" href="#">RESEARCH TEAM</a></li>
+          <li><a class="dropdown-item" style="color: black" href="#">DEVELOPERS</a></li>
+          </ul>
+        </li>
+        <a class="footer-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Publications
+          </a>
+          <ul class="dropdown-menu">
+          <li><a class="dropdown-item" style="color: black" href="#" >Publication1</a></li>
+          <li><a class="dropdown-item" style="color: black" href="#">Publication2</a></li>
+          <li><a class="dropdown-item" style="color: black" href="#">Publication3</a></li>
+          </ul>
+        </li>
+          <a href="#">Archives</a>
+          <a href="#">Editorial</a>
+          <a href="#">Announcements</a>
+          <a class="footer-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Publish
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" style="color: black" href="#">Publish1</a></li>
+            <li><a class="dropdown-item" style="color: black" href="#">Publish2</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" style="color: black" href="#">Publish3</a></li>
+          </ul>
+        </li>
+        </div>
+        </div>
+      </div>
+      </div>
+
+
+    
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" ></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+      <script src="../JS/home.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+</body>
+</html>
--- PHP/home.php
+++ PHP/home.php
@@ -57,6 +57,7 @@
       </div> -->
 
 </div>
+<?php include 'footer.php' ?>
 
 
 
