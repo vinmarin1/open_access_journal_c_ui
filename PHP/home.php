36a85997d02773a26e60143166c4e8db44b3cbbe
Stephen Heraldo <stephenzionheraldo@gmail.com>
2023-11-08 10:35:30 +0800

    Updated UI for home

--- PHP/home.php
+++ PHP/home.php
@@ -43,11 +43,10 @@
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur facilis nemo culpa quis iste itaque deserunt magnam voluptate voluptatem? Necessitatibus sint voluptatibus non? Ipsum vero omnis quas, nobis ex qui ipsa recusandae! Mollitia eaque eveniet ullam consequatur quibusdam, odit adipisci architecto accusamus a animi facere pariatur, deserunt veniam fugiat in.</p>
         <button class="btn btn-outline-light btn-md">Continue Reading</button>
         </div>
-      
-      
       </div>
      
     </div>
+
     <!-- <div class="articles-container">
         <div class="articles">
             <div class="article1">
@@ -56,14 +55,72 @@
         </div>
       </div> -->
 
+
+<div class="fluid-container mb-3 qoaj">
+  <div class="About-container">
+    <div class="ab-qoaj-left">
+        <h2 class="mb-3">About QOAJ</h2>
+        <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis distinctio, debitis sed dolores iste, deserunt perspiciatis ducimus odio aliquam facere illo, quasi temporibus aut sint est mollitia saepe omnis amet?</p>
+        <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis distinctio, debitis sed dolores iste, deserunt perspiciatis ducimus odio aliquam facere illo, quasi temporibus aut sint est mollitia saepe omnis amet?</p>
+        <br>
+          <button class="btn btn-primary btn-md mt-1">Read More</button>
+        <br>
+    </div>
+  
+    <div class="ab-qoaj-right">
+      <div class="text1">
+      <h4>1,243</h4>
+      <p>Articles published</p>
+      </div>
+      <div class="divider-line-2"></div>
+
+      <div class="text1">
+      <h4>89</h4>
+      <p>Total Contribution</p>
+      </div>
+      <div class="divider-line-2"></div>
+
+      <div class="text1">
+      <h4>12,093</h4>
+      <p>Page Visitors</p>
+      </div>
+      <div class="divider-line-2"></div>
+
+      <div class="text1">
+      <h4>3,668</h4>
+      <p>Articles Downloads</p>
+      </div>
+    </div>
+  </div>
+  <img src="../images/Papers.png" alt="#" class="image">
 </div>
-<?php include 'footer.php' ?>
+<div class="container-fluid">
+  <div class="ex-journal">
+    <div class="example-journal">
+    <h3>The Gavel</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt quidem illum repellat dolores voluptas in deserunt velit minima, dolorem laborum impedit, nostrum ab dolor possimus autem fuga tempora officia non quasi nisi voluptatem. Pariatur magni impedit, enim in fugiat quos.</p>
+    <button class="btn btn-primary btn-md mt-1">See Journal</button>
+    </div>
+
+    <div class="example-journal" id="example-mid">
+    <h3>The Star</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt quidem illum repellat dolores voluptas in deserunt velit minima, dolorem laborum impedit, nostrum ab dolor possimus autem fuga tempora officia non quasi nisi voluptatem. Pariatur magni impedit, enim in fugiat quos.</p>
+    <button class="btn btn-primary btn-md mt-1">See Journal</button>
+    </div>
+
+    <div class="example-journal">
+    <h3>The Lamp</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt quidem illum repellat dolores voluptas in deserunt velit minima, dolorem laborum impedit, nostrum ab dolor possimus autem fuga tempora officia non quasi nisi voluptatem. Pariatur magni impedit, enim in fugiat quos.</p>
+    <button class="btn btn-primary btn-md mt-1">See Journal</button>
+    </div>
+  </div>
+</div>
+</div>
 
 
+<?php include 'footer.php'?>
 
 
-
-   
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" ></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
