6d73c4877722f4cbaaf080295f9fee1efcd89589
dharren <dharren.sanchez023@gmail.com>
2023-12-03 17:53:17 +0800

    new modified author dashboard

---
+++ JS/author-dashboard.js
@@ -0,0 +1,61 @@
+// Your JavaScript for Chart.js and 'Add New' functionality
+window.onload = function() {
+    var ctx = document.getElementById('articlesChart').getContext('2d');
+    var chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+          label: 'Views',
+          backgroundColor: 'rgba(10, 51, 91, 0.2)', // Updated to your color in rgba
+          borderColor: '#0A335B', // Updated to your color in hex
+          pointBackgroundColor: '#0A335B', // Point color
+          pointBorderColor: '#0A335B', // Point border color
+          pointRadius: 5, // Adjust the radius as needed
+          pointHoverRadius: 7, // Radius on hover
+          pointStyle: 'circle', // Style of the point
+          data: [0, 300, 200, 320, 450, 325, 400, 430, 450, 500, 550, 500],
+          fill: false,
+        },
+        {
+          label: 'Downloads',
+          backgroundColor: 'rgba(229, 111, 31, 0.2)', // Updated to your color in rgba
+          borderColor: '#E56F1F', // Updated to your color in hex
+          pointBackgroundColor: '#E56F1F', // Point color
+          pointBorderColor: '#E56F1F', // Point border color
+          pointRadius: 5, // Adjust the radius as needed
+          pointHoverRadius: 7, // Radius on hover
+          pointStyle: 'circle', // Style of the point
+          data: [0, 200, 300, 350, 400, 420, 410, 480, 400, 430, 570, 600],
+          fill: false,
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        legend: {
+          labels: {
+            usePointStyle: true // Use point style for legend icons too
+          }
+        }
+      }
+    });
+  };
+  
+    // Get all tabs
+    var tabs = document.querySelectorAll('.tab');
+    
+    // Add click event listener to each tab
+    tabs.forEach(function(tab) {
+      tab.addEventListener('click', function() {
+        // Remove active class from all tabs
+        tabs.forEach(function(tab) {
+          tab.classList.remove('active');
+        });
+        // Add active class to clicked tab
+        this.classList.add('active');
+      });
+    });
--- PHP/author-dashboard.php
+++ PHP/author-dashboard.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QCU PUBLICATION | AUTHOR DASHBOARD</title>
-    <link rel="stylesheet" href="../CSS/author-dashboard.css">
+    <link rel="stylesheet" href="../CSS/author_dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -38,127 +38,98 @@ session_start();
 
 
         </div>
-        <button class="btn tbn-primary btn-md" id="btn2" onclick="window.location.href='author-dashboard.php'">As Author</button>
+        <button class="btn tbn-primary btn-md" id="btn1" onclick="window.location.href='author-dashboard.php'">As Author</button>
         <button class="btn tbn-primary btn-md" id="btn2" onclick="window.location.href='reviewer-dashboard.php'">As Reviewer</button>
     </div>
-    <div class="dashboard">
-  <div class="status-boxes">
-    <div class="status-box pending">
-      <div class="status-label">Pending</div>
-      <div class="status-value">0</div>
-    </div>
-    <div class="status-box publish">
-      <div class="status-label">Publish</div>
-      <div class="status-value">0</div>
-    </div>
-    <div class="status-box declined">
-      <div class="status-label">Declined</div>
-      <div class="status-value">0</div>
-    </div>
-    <div class="status-box total-views">
-      <div class="status-label">Total Views</div>
-      <div class="status-value">0</div>
-    </div>
-  </div>
-  <div class="contribution-section">
-    <div class="contribution-text">
-      <h2>Be the best Author and be our Contributor</h2>
-      <p>All of our journals are handled by dedicated Editors who are active in the specific communities they serve and are discoverable in the COU students and others School to spread related articles of what they are looking for.</p>
-    </div>
-	<div class="vertical-line"></div>
-    <div class="contribution-actions">
-    <button class="rounded-button" onclick="window.location.href='browse-articles.php'">Browse Articles</button>
-    <button class="rounded-button" onclick="window.location.href='timeline.php'">Be a Contributer</button>
-    <button class="rounded-button " onclick="window.location.href='reviewer-dashboard.php'">For Reviewer</button>
-    </div>
-  </div>
+<div class="main">
+    <div class="row">
+<div class="graph-section">
+  <!-- Graph placeholder -->
+  <h3>Published Articles Engagement</h3>
+  <canvas id="articlesChart" width="400" height="200"></canvas>
 </div>
-
-
-<div class="fluid-container">
-<div class="recommendation-article">
-<h4><strong>Your Published Articles</strong></h4>
-  <div class="articles-container">
-  <div class="article">
-  <p class="h6">Blockchain Beyond Cyptocurrency: Transforming Industries with Distributed Ledger Technology</p>
-    <div class="article-info">
-      <p class="info">THE LAMP</p>
-      <span class="views"></span>103 views
-    </div>
-    <p class="author">By Jane Delacruz</p>
-    <p class="article-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sint facilis nihil possimus, illum ullam. Illo voluptatem totam repellendus voluptas.</p>
-    <button class="btn btn-primary btn-md btn-article"  style=" border: 2px #115272 solid;
-    background-color: transparent;
-    border-radius: 20px;
-    color: #115272;
-    width: 100%;">Read Article</button>
+<div class="stats-section">
+  <!-- Top row cards -->
+  <div class="stat-card top-card">
+    <h2>Total Views</h2>
+    <p>98 <span class="increase">+11%</span></p>
   </div>
-  <div class="article">
-  <p class="h6">Blockchain Beyond Cyptocurrency: Transforming Industries with Distributed Ledger Technology</p>
-    <div class="article-info">
-      <p class="info">THE LAMP</p>
-      <span class="views"></span>103 views
-    </div>
-    <p class="author">By Jane Delacruz</p>
-    <p class="article-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sint facilis nihil possimus, illum ullam. Illo voluptatem totam repellendus voluptas.</p>
-    <button class="btn btn-primary btn-md btn-article"  style=" border: 2px #115272 solid;
-    background-color: transparent;
-    border-radius: 20px;
-    color: #115272;
-    width: 100%;">Read Article</button>
+  <div class="stat-card top-card">
+    <h2>Total Downloads</h2>
+    <p>98 <span class="increase">+11%</span></p>
   </div>
-
-  <div class="article">
-  <p class="h6">Blockchain Beyond Cyptocurrency: Transforming Industries with Distributed Ledger Technology</p>
-    <div class="article-info">
-      <p class="info">THE LAMP</p>
-      <span class="views"></span>103 views
-    </div>
-    <p class="author">By Jane Delacruz</p>
-    <p class="article-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sint facilis nihil possimus, illum ullam. Illo voluptatem totam repellendus voluptas.</p>
-    <button class="btn btn-primary btn-md btn-article"  style=" border: 2px #115272 solid;
-    background-color: transparent;
-    border-radius: 20px;
-    color: #115272;
-    width: 100%;">Read Article</button>
-  </div>
-
-  <div class="article">
-  <p class="h6">Blockchain Beyond Cyptocurrency: Transforming Industries with Distributed Ledger Technology</p>
-    <div class="article-info">
-      <p class="info">THE LAMP</p>
-      <span class="views"></span>103 views
-    </div>
-    <p class="author">By Jane Delacruz</p>
-    <p class="article-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sint facilis nihil possimus, illum ullam. Illo voluptatem totam repellendus voluptas.</p>
-    <button class="btn btn-primary btn-md btn-article"  style=" border: 2px #115272 solid;
-    background-color: transparent;
-    border-radius: 20px;
-    color: #115272;
-    width: 100%;">Read Article</button>
-  </div>
-
-  <div class="article">
-  <p class="h6">Blockchain Beyond Cyptocurrency: Transforming Industries with Distributed Ledger Technology</p>
-    <div class="article-info">
-      <p class="info">THE LAMP</p>
-      <span class="views"></span>103 views
-    </div>
-    <p class="author">By Jane Delacruz</p>
-    <p class="article-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sint facilis nihil possimus, illum ullam. Illo voluptatem totam repellendus voluptas.</p>
-    <button class="btn btn-primary btn-md btn-article"  style=" border: 2px #115272 solid;
-    background-color: transparent;
-    border-radius: 20px;
-    color: #115272;
-    width: 100%;">Read Article</button>
-  </div>
-
   
+  <!-- Bottom row cards -->
+<div class="stat-card bottom-card">
+  <div class="content">
+    <h2>Most Popular</h2>
+    <p>Blockchain Beyond...</p>
+  </div>
+  <p>192 interactions</p>
+</div>
+<div class="stat-card bottom-card">
+  <h2>Total Published</h2>
+  <p>02</p>
+</div>
+</div>
+  </div>
+  <hr class="full-width">
+<div class="row">
+<div class="articles-section full-width">
+  <div class="tabs">
+    <div class="tab active">All Submissions</div>
+    <div class="tab">Published</div>
+    <div class="tab">Not Published</div>
+    <button class="btn" id="btn3">Add New</button>
+  </div>
   
+  <table>
+    <thead>
+      <tr>
+        <th><input type="checkbox"></th>
+        <th>Title</th>
+        <th>Date</th>
+        <th>Journal</th>
+        <th><center>Status</center></th>
+        <th><center>Action</center></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><input type="checkbox"></td>
+        <td>Blockchain Beyond Cryptocurrency: Transforming...</td>
+        <td>May 31, 2015</td>
+        <td>The Star</td>
+        <td><center><span class="status-label pending">Pending</span></center></td>
+        <td><center>...</center></td>
+      </tr>
+      <tr>
+        <td><input type="checkbox"></td>
+        <td>Industries with Distributed Ledger Technology</td>
+        <td>October 24, 2018</td>
+        <td>The Star</td>
+        <td><center><span class="status-label published">Published</span></center></td>
+        <td><center>...</center></td>
+      </tr>
+    </tbody>
+  </table>
+<hr class="full-width">
+<div class="pagination">
+  Showing 1 to 10 of 50 entries
+  <div class="pagination-controls">
+    <button>«</button>
+    <button>‹</button>
+    <button class="active">1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>›</button>
+    <button>»</button>
   </div>
 </div>
 </div>
-
+</div>
+</div>
 
 
 <div class="footer" id="footer">
@@ -169,5 +140,7 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
     <script src="../JS/reusable-header.js"></script>
+    <script src="../JS/author-dashboard.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
