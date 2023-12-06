6477418d4863a366a4948681e1c5affe090c2d69
Mon Cedric Avila <moncedricavila@gmail.com>
2023-12-06 20:48:57 +0800

    crud in add user

--- Admin/php/userandroleslist.php
+++ Admin/php/userandroleslist.php
@@ -49,8 +49,8 @@ $rolelist = get_role_list();
                                 <td width="50%"><?php echo $userlistval->last_name; ?>, <?php echo $userlistval->first_name; ?></td>
                                 <td width="50%"><?php echo $userlistval->email; ?></td>
                                 <td width="10%">
-                                    <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#updateModal">Update</button>
-                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#archiveModal">Archive</button>
+                                    <button type="button" class="btn btn-outline-success" onclick="updateModal(<?php echo $userlistval->author_id; ?>)">Update</button>
+                                    <button type="button" class="btn btn-outline-danger" onclick="archiveUser(<?php echo $userlistval->author_id; ?>, '<?php echo $userlistval->first_name; ?>', '<?php echo $userlistval->last_name; ?>')">Archive</button>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
@@ -84,8 +84,8 @@ $rolelist = get_role_list();
                                 <td width="35%"><?php echo $rolelistval->role; ?></td>
                                 <td width="50%"><?php echo $rolelistval->role_name; ?></td>
                                 <td width="10%">
-                                    <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#updateModal">Update</button>
-                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#archiveModal">Archive</button>
+                                    <button type="button" class="btn btn-outline-success" onclick="openUpdateModal('<?php echo $rolelistval->admin_role_id; ?>')">Update</button>
+                                    <button type="button" class="btn btn-outline-danger" onclick="openArchiveModal('<?php echo $rolelistval->admin_role_id; ?>')">Archive</button>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
@@ -129,6 +129,159 @@ $rolelist = get_role_list();
                 "searching": true,
             });
         });
+
+        function addRecord() {
+        var formData = {
+            first_name: $("#first_name").val(),
+            middle_name: $("#middle_name").val(),
+            last_name: $("#last_name").val(),
+            email: $("#email").val(),
+            phone_number: $("#phone_number").val(),
+            gender: $("#gender").val(),
+            dob: $("#dob").val(),
+            orcid: $("#orcid").val(),
+            orcidurl: $("#orcidurl").val(),
+            school_name: $("#school_name").val(),
+            field_expertise: $("#field_expertise").val(),
+            action: "add"
+        };
+
+        $.ajax({
+            url: "userandroles_function.php",
+            method: "POST",
+            data: formData,
+            success: function (data) {
+                var response = JSON.parse(data);
+
+                // Show alert
+                if (response.status) {
+                    alert("Record added successfully");
+                } else {
+                    alert("Failed to add record");
+                }
+
+                // Reload the page
+                location.reload();
+            },
+            error: function (xhr, status, error) {
+                console.error("Ajax request failed:", error);
+            }
+        });
+    }
+
+    function archiveUser(authorId, firstName, lastName) {
+        $('#archiveModal').modal('show');
+        $('#archiveModalTitle').text('Archive User');
+        $('#userInfo').html('<strong>Name:</strong> ' + lastName + ', ' + firstName + '<br><strong>ID:</strong> ' + authorId);
+
+        $('#archiveModalSave').off().on('click', function () {
+            $.ajax({
+                url: "userandroles_function.php",
+                method: "POST",
+                data: { action: "archive", author_id: authorId },
+                success: function (data) {
+                    var response = JSON.parse(data);
+
+                    if (response.status) {
+                        $('#archiveModalMessage').text('User archived successfully');
+                    } else {
+                        $('#archiveModalMessage').text('Failed to archive user');
+                    }
+                        $('#archiveModal').modal('hide');
+                        location.reload();
+                },
+                error: function (xhr, status, error) {
+                    console.error("Ajax request failed:", error);
+                    $('#archiveModalMessage').text('Failed to archive user');
+                    $('#archiveModal').modal('hide');
+                    location.reload();
+                }
+            });
+        });
+    }
+
+    function updateModal(authorId) {
+        $.ajax({
+            type: 'POST',
+            url: 'userandroles_function.php',
+            data: { action: 'fetch', author_id: authorId },
+            dataType: 'json',
+            success: function (response) {
+                console.log('Response from server:', response);
+
+                if (response.status === true && response.data.length > 0) {
+                    const userData = response.data[0];
+                    console.log('User Data:', userData);
+
+                    $('#xauthorid').val(userData.author_id);
+                    $('#xfirst_name').val(userData.first_name);
+                    $('#xmiddle_name').val(userData.middle_name);
+                    $('#xlast_name').val(userData.last_name);
+                    $('#xemail').val(userData.email);
+                    $('#xphone_number').val(userData.phone_number);
+                    $('#xgender').val(userData.gender);
+                    $('#xdob').val(userData.birth_date);
+                    $('#xorcid').val(userData.orc_id);
+                    $('#xorcidurl').val(userData.url_orc_id);
+                    $('#xschool_name').val(userData.school_name);
+                    $('#xfield_expertise').val(userData.field_of_expertise);
+
+                    // Show the modal
+                    $('#updateModal').modal('show');
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('AJAX Error:', textStatus, errorThrown);
+                // Handle AJAX errors
+                console.log('Error fetching user data');
+                // You can perform additional actions here if needed
+            }
+        });
+    }
+
+    function saveChanges() {
+        console.log('Save button clicked');
+        
+        var authorId = $('#xauthorid').val();
+        var updatedData = {
+            first_name: $('#xfirst_name').val(),
+            middle_name: $('#xmiddle_name').val(),
+            last_name: $('#xlast_name').val(),
+            email: $('#xemail').val(),
+            phone_number: $('#xphone_number').val(),
+            gender: $('#xgender').val(),
+            dob: $('#xdob').val(),
+            orc_id: $('#xorcid').val(),
+            url_orc_id: $('#xorcidurl').val(),
+            school_name: $('#xschool_name').val(),
+            field_of_expertise: $('#xfield_expertise').val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: 'userandroles_function.php',
+            data: {
+                action: 'update',
+                author_id: authorId,
+                updated_data: updatedData
+            },
+            dataType: 'json',
+            success: function (response) {
+                console.log('Update Response:', response);
+
+                if (response.status === true) {
+                    // Handle successful update
+                    alert("Record updated successfully");
+                    $('#updateModal').modal('hide');
+                    location.reload();
+                } else {
+                    // Handle update failure
+                    console.error('Error updating user data:', response.message);
+                    alert("Failed to update record. Please try again.");
+                }
+            },
+        });
+    }
     </script>
 
      <!-- Add Modal -->
@@ -217,7 +370,7 @@ $rolelist = get_role_list();
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary" onclick="addRecord()">Save changes</button>
                 </div>
             </div>
         </div>
@@ -234,39 +387,40 @@ $rolelist = get_role_list();
                 <div class="modal-body">
                     <div class="row mb-2">
                         <div class="col-md-4 mb-2">
-                            <label for="xfirstname" class="form-label">First Name</label>
-                            <input type="text" id="first_name" class="form-control" placeholder="First Name" />
+                            <input type="hidden" id="xauthorid" class="form-control"/>
+                            <label for="first_name" class="form-label">First Name</label>
+                            <input type="text" id="xfirst_name" class="form-control" placeholder="First Name" />
                         </div>
                         <div class="col-md-4 mb-2">
-                            <label for="xmiddlename" class="form-label">Middle Name</label>
-                            <input type="text" id="middle_name" class="form-control" placeholder="Middle Name" />
+                            <label for="middle_name" class="form-label">Middle Name</label>
+                            <input type="text" id="xmiddle_name" class="form-control" placeholder="Middle Name" />
                         </div>
                         <div class="col-md-4 mb-2">
-                            <label for="xlastname" class="form-label">Last Name</label>
-                            <input type="text" id="last_name" class="form-control" placeholder="Last Name" />
+                            <label for="last_name" class="form-label">Last Name</label>
+                            <input type="text" id="xlast_name" class="form-control" placeholder="Last Name" />
                         </div>
                     </div>
                     <div class="row mb-2">
                         <div class="col-md-6 mb-2">
-                            <label class="form-label" for="xemail">Email</label>
+                            <label class="form-label" for="email">Email</label>
                             <div class="input-group input-group-merge">
                                 <span class="input-group-text"><i class="bx bx-envelope"></i></span>
-                                <input type="text" id="email" class="form-control" placeholder="juan.delacruz" aria-label="juan.delacruz" aria-describedby="basic-icon-default-email2" />
+                                <input type="text" id="xemail" class="form-control" placeholder="juan.delacruz" aria-label="juan.delacruz" aria-describedby="basic-icon-default-email2" />
                                 <span id="semail" class="input-group-text">@example.com</span>
                             </div>
                         </div>
                         <div class="col-md-6 mb-2">
-                            <label class="form-label" for="xphone_number">Phone No</label>
+                            <label class="form-label" for="phone_number">Phone No</label>
                             <div class="input-group input-group-merge">
                                 <span id="sphone_number" class="input-group-text"><i class="bx bx-phone"></i></span>
-                                <input type="text" id="phone_number" class="form-control phone-mask" placeholder="639 799 8941" aria-label="639 799 8941" aria-describedby="basic-icon-default-phone2" />
+                                <input type="text" id="xphone_number" class="form-control phone-mask" placeholder="639 799 8941" aria-label="639 799 8941" aria-describedby="basic-icon-default-phone2" />
                             </div>
                         </div>
                     </div>
                     <div class="row mb-2">
                         <div class="col-md-6 mb-2">
-                            <label for="xgender" class="form-label">Gender</label>
-                            <select id="gender" class="form-select">
+                            <label for="gender" class="form-label">Gender</label>
+                            <select id="xgender" class="form-select">
                                 <option>Select Gender</option>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
@@ -274,58 +428,60 @@ $rolelist = get_role_list();
                             </select>
                         </div>
                         <div class="col-md-6 mb-2">
-                            <label for="xdob" class="form-label">DOB</label>
-                            <input type="date" id="dob" class="form-control" />
+                            <label for="dob" class="form-label">DOB</label>
+                            <input type="date" id="xdob" class="form-control" />
                         </div>
                     </div>
                     <div class="row mb-2">
                         <div class="col-md-6 mb-2">
-                            <label for="xorcid" class="form-label">ORCID</label>
-                            <input type="text" id="orcid" class="form-control" placeholder="ORCID" />
+                            <label for="orcid" class="form-label">ORCID</label>
+                            <input type="text" id="xorcid" class="form-control" placeholder="ORCID" />
                         </div>
                         <div class="col-md-6 mb-2">
-                            <label for="xorcidurl" class="form-label">ORCID URL</label>
-                            <input type="text" id="orcidurl" class="form-control" placeholder="ORCID URL" />
+                            <label for="orcidurl" class="form-label">ORCID URL</label>
+                            <input type="text" id="xorcidurl" class="form-control" placeholder="ORCID URL" />
                         </div>
                     </div>
                     <div class="row mb-2">
                         <div class="col-md-6 mb-2">
-                            <label for="xschoolname" class="form-label">SCHOOL NAME</label>
-                            <input type="text" id="school_name" class="form-control" placeholder="SCHOOL NAME" />
+                            <label for="school_name" class="form-label">SCHOOL NAME</label>
+                            <input type="text" id="xschool_name" class="form-control" placeholder="SCHOOL NAME" />
                         </div>
                         <div class="col-md-6 mb-2">
-                            <label for="xexpertise" class="form-label">EXPERTISE</label>
-                            <input type="text" id="field_expertise" class="form-control" placeholder="EXPERTISE" />
+                            <label for="field_expertise" class="form-label">EXPERTISE</label>
+                            <input type="text" id="xfield_expertise" class="form-control" placeholder="EXPERTISE" />
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary" id="updateModalSave" onclick="saveChanges()">Save changes</button>
                 </div>
             </div>
         </div>
-    </div>  
+    </div>
 
      <!-- Archive Modal -->
      <div class="modal fade" id="archiveModal" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">Archive User</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title" id="archiveModalTitle">Archive User</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                        <h5 class="modal-title" id="modalToggleLabel">Are you sure you want to archive user 1?</h5>
+                        <h5 class="modal-title" id="modalToggleLabel">Are you sure you want to archive the following user?</h5>
+                        <p id="userInfo"></p>
                     </div>
                 </div>
                 <div class="modal-footer">
-                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="archiveModalSave">Save changes</button>
                 </div>
             </div>
         </div>
     </div>
+
 </body>
 </html>
