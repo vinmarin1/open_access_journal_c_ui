55a3d3e3745d390c9c62c111eea3e581e8974983
dharren <dharren.sanchez023@gmail.com>
2023-12-23 02:38:39 +0800

    submitted article details UI

---
+++ PHP/submitted-article.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Submitted Article</title>
+<link href="../CSS/submitted-article.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+
+<body>
+
+<div class="header-container" id="header-container">
+</div>
+
+<nav class="navigation-menus-container"  id="navigation-menus-container">
+</nav>
+
+
+<div class="article-title">
+  <!-- Header content like title and authors goes here -->
+    <div class="main-title">
+        <div class="breadcrumbs">
+            Dashboard / Author / Submitted Articles
+        </div>
+    A Classroom-based Action Research on Selected First Year Information Technology Students of Quezon City University for the Second Semester of Academic Year 2021-2022
+    </div>
+</div>
+<div class="row1">
+    <div class="abstract">
+    <!-- Abstract content goes here -->
+        <div class="abstract-title">Abstract</div>
+        <div class="abstract-content">
+            After fighting the COVID-19 pandemic for more than two years, food production could not keep up with the rapid population expansion, which led to increased food prices and the prevalence of hunger and poverty. In this feasibility project, the company's primary objective, GAIAnova Inc., is to innovate a mobile application to help the environment produce fresh vegetables at home called GAIAnova Mobile Application. It offers a variety of information, knowledge, and step by step procedures regarding urban farming, types of urban farming, plant diseases, water mixtures, pest control, etc., to promote urban farming in Quezon City. The objective of the market study was to determine self-sufficient gardening as a revolutionary and profitable application for urban farming using the biological and knowledge gap. The market study was conducted with a total respondent of 100-200 every barangay in Quezon City. The findings showed a 97.7% acceptance rate from participants, and involved demands sums $37,234 for 2023. As a result of the rapid growth in technology and the fact that most people like to use their mobile phones, researchers believe it would be advantageous to create a product that readily conveys the value of urban farming and the advantages it can provide to people. Due to the rapid growth of technology, the researchers see it as a benefit to produce a product that easily explains the value of urban farming and its benefits to people. Furthermore, this research aims to produce a groundbreaking mobile application that will be valuable for students, farmers, agriculture enthusiasts, and plant lovers.
+        </div>
+    </div>
+    <div class="column1">
+
+        <div class="status">
+            <span class="status-label review">Review</span>
+        </div>
+
+        <div class="submit-details">
+            <!-- Submission details like dates and IDs goes here -->
+            <div class="submit-section">
+                <div class="submit-section-title">Submitted in The Gavel</div>
+            </div>
+            <div class="logs-date-container">
+                <div class="logs">
+                    
+                    <div class="logs-title">Logs</div> <!-- Logs title -->
+                    <div class="log-entry"><b>Review</b><br>1st version revision</div>
+                    <div class="log-entry"><b>Review</b><br>Reviewed by Reviewer B</div>
+                    <div class="log-entry"><b>Review</b><br>Reviewed by Reviewer A</div>
+                    <div class="log-entry"><b>Review</b><br>Send for review</div>
+                    <div class="log-entry"><b>Submission</b><br>Submitted in the Gavel</div>
+                </div>
+                
+                <div class="dates">
+                    <div class="dates-title">Date</div>
+                    <div class="date">2023-11-09</div>
+                    <div class="date">2023-11-09</div>
+                    <div class="date">2023-11-09</div>
+                    <div class="date">2023-11-09</div>
+                    <div class="date">2023-11-09</div>
+                </div>
+            </div>
+            <a href="#" class="view-all-logs">View All Logs</a>
+        </div>
+    </div>
+</div> 
+
+<div class="row2">
+    <div class="main2">
+        <div class="files-submitted">
+            <div class="table-header">Files Submitted</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>File</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Article AboutThe Future of Artificial Intelligence_v1.pdf</td>
+                        <td>2023-11-09</td>
+                    </tr>
+                    <tr>
+                        <td>Article AboutThe Future of Artificial Intelligence.pdf</td>
+                        <td>2023-11-09</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="files-submitted">
+            <div class="table-header">Contributors</div>
+                <table>
+                    <tr>
+                        <th>File name</th>
+                        <th>ORCID</th>
+
+                    </tr>
+                    <tr>
+                        <td>Jane Doe</td>
+                        <td>04923045324</td>
+
+                    </tr>
+                    <tr>
+                        <td>Juan Dela Cruz</td>
+                        <td>04923045324</td>
+
+                    </tr>
+                </table>
+                <div class="keywords">
+                    <span>Keywords</span><br><br>
+                    <span class="keyword-tag">technology</span>
+                    <span class="keyword-tag">covid-19</span>
+                    <!-- Repeat the keyword tags as necessary -->
+                </div>
+        </div>
+    </div>
+    <div class="main3">
+        <div class="comments-container">
+            <div class="table-header">Comments</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Comment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>For revision</td>
+                    </tr>
+                    <tr>
+                        <td>Comment Comment Comment Comment Comm...</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="review-rubrics">
+            <div class="section-header">Review Rubrics</div>
+            <div class="section-body">
+                <p>In the web browser: Does the Little Lemon web app match the UI/UX from the Figma mockup and wireframe?</p>
+                <form>
+                    <ul>
+                        <li>
+                            <label>
+                                <input type="radio" name="question1" value="1">
+                                1 point <span>A</span>
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="question1" value="0">
+                                0 points <span>B</span>
+                            </label>
+                        </li>
+                    </ul>
+                </form>
+
+                <p>Using the web browser developer tools, does the web app use semantic HTML tags appropriately?</p>
+                <form>
+                    <ul>
+                        <li>
+                            <label>
+                                <input type="radio" name="question2" value="1">
+                                1 point <span>A</span>
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="question2" value="0">
+                                0 points <span>B</span>
+                            </label>
+                        </li>
+                    </ul>
+                </form>
+
+                <p>Does the web app layout correctly for both mobile and desktop devices?</p>
+                <form>
+                    <ul>
+                        <li>
+                            <label>
+                                <input type="radio" name="question3" value="1">
+                                1 point <span>A</span>
+                            </label>
+                        </li>
+                        <li>
+                            <label>
+                                <input type="radio" name="question3" value="0">
+                                0 points <span>B</span>
+                            </label>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <div class="action-button">
+                <button type="button" class="btn btn-primary btn-md" id="edit-submission">Edit Submission</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<div class="footer" id="footer">
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+<script src="../JS/reusable-header.js"></script>
+<script src="../JS/submitted-article.js"></script>
+
+
+</body>
+</html>
+
