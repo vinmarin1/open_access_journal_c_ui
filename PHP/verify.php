15eef2cf4b28eb0702148213d173eadc970d6ef4
vinmarin1 <marvin.gozo23@gmail.com>
2023-12-24 00:27:07 +0800

    testing 11 deployment

--- PHP/verify.php
+++ PHP/verify.php
@@ -74,7 +74,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QCU TIMES PUBLICATION | VERIFICATION CODE</title>
-	<link rel="stylesheet" href="../css/verify.css">
+	<link rel="stylesheet" href="../CSS/verify.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 	
