24558c0383dedbfaf6ce952f9c7ccaea6c7a9a7b
Mon Cedric Avila <moncedricavila@gmail.com>
2023-12-23 17:43:15 +0800

    updated workflow

--- Admin/php/function/email_function.php
+++ Admin/php/function/email_function.php
@@ -120,7 +120,27 @@ if (!function_exists('get_email_content')) {
         }
     }
     
-
+    if (!function_exists('get_reviewer_content')) {
+        function get_reviewer_content($emc) {
+            $pdo = connect_to_database();
     
-
+            if ($pdo) {
+                try {
+                    $query = "SELECT * FROM email_content WHERE id = :emc";
+                    $stmt = $pdo->prepare($query);
+                    $stmt->bindParam(':emc', $emc, PDO::PARAM_INT);
+                    $stmt->execute();
+    
+                    $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+    
+                    return $result;
+                } catch (PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                    return false;
+                }
+            }
+    
+            return false;
+        }
+    }
 ?>
