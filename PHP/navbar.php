9501223b610f04312e93d790a78cff6d4d109c94
vinmarin1 <marvin.gozo23@gmail.com>
2023-12-23 23:59:13 +0800

    testing 6 deployment

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -1,4 +1,6 @@
-
+<?php
+   session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,7 +51,7 @@
       </form>
       
       <?php
-        session_start();
+     
         if (isset($_SESSION['LOGGED_IN']) && $_SESSION['LOGGED_IN'] === true) {
 
         } else {
