4d9d974eed71ca6ef6b1238ed3b45ff31d728236
Mon Cedric Avila <moncedricavila@gmail.com>
2023-12-24 13:43:03 +0800

    updated

--- Admin/php/template/header.php
+++ Admin/php/template/header.php
@@ -9,8 +9,7 @@ $journal = get_journal_list();
 $author_id = isset($_SESSION['id']) ? $_SESSION['id'] : 0;
 
 if (empty($author_id)) {
-    header('Location:../../index.php');
-    exit();
+    echo '<script>window.location.href = "../../index.php";</script>';
 }
 ?>
 
