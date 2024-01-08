07fd1f9780302b212f9766146c554425e1047050
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-01-08 13:04:08 +0800

    feat: sort most popular home

---
+++ JS/recently-added-api.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', fetchData);
+
+
+function navigateToArticle(articleId){
+  window.location.href = `../PHP/article-details.php?articleId=${articleId}`;
+}
+
+async function fetchData() {
+  try {
+    const response = await fetch('https://web-production-cecc.up.railway.app/api/articles/?sort=recently-added', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "period": ""
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log('API Response:', data);
+
+    // Assuming 'data.recommendations' is an array
+    const articlesContainer = document.querySelector('#popular-articles');
+    
+
+    data.results.forEach(item => {
+      const articleDiv = document.createElement('div');
+      articleDiv.classList.add('article');
+      articleDiv.addEventListener('click', () => navigateToArticle(item.article_id));
+      let contributorsHTML = "";
+      if (item.contributors != null) {
+        for (const contributors of item.contributors.split(",")) {
+          contributorsHTML += `<a href="https://orcid.org/${contributors.split("-")[1]}">${contributors.split("-")[0]}</a>,`;
+        }
+      }
+      articleDiv.innerHTML = `
+        <p class="h6" id="title">${item.title}</p>
+        <div class="article-info">
+          <p class="info" id="category">${item.journal || 'No Journal'}</p>
+          <p class="">${new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(new Date(item.date_added))}</p>
+        </div>
+        <p class="article-content" id="abstract">${item.abstract.slice(0,100)}...</p>
+        <button class="btn btn-primary btn-md btn-article" style="border: 2px #0858a4 solid; background-color: transparent; border-radius: 20px; color: #0858a4; width: 100%;">Read Article</button>
+      `;
+
+      articlesContainer.appendChild(articleDiv);
+    });
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    // You can handle errors or display a message as needed
+  }
+}
+
