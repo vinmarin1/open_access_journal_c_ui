a750d88d3734570a9aa3a1228fabbc5b94cd971b
Mon Cedric Avila <moncedricavila@gmail.com>
2024-01-19 12:07:07 +0800

    reports

--- Admin/php/function/report_function.php
+++ Admin/php/function/report_function.php
@@ -23,4 +23,44 @@ if (!function_exists('get_report_list')) {
         return false;
     }
 }
+
+if (!function_exists('get_archive_article_list')) {
+    function get_archive_article_list($month, $year) {
+        $month = $_GET["m"] ?? date('m');
+        $year = $_GET["y"] ?? date('Y');
+
+        $sql = "SELECT `article_id`, `title`, `archive_date` FROM `article` WHERE `status` = 0 AND MONTH(`archive_date`) = ? AND YEAR(`archive_date`) = ?";
+
+        $results = execute_query($sql, [$month, $year]);
+
+        $data = array();
+
+        if ($results !== false) {
+            $data['articlelist'] = $results;
+            return $data;
+        } else {
+            return array('status' => true, 'data' => []);
+        }
+    }
+}
+
+if (!function_exists('get_published_article_list')) {
+    function get_published_article_list($month, $year) {
+        $month = $_GET["m"] ?? date('m');
+        $year = $_GET["y"] ?? date('Y');
+
+        $sql = "SELECT `article_id`, `title`, `publication_date` FROM `article` WHERE `status` = 1 AND MONTH(`publication_date`) = ? AND YEAR(`publication_date`) = ?";
+
+        $results = execute_query($sql, [$month, $year]);
+
+        $data = array();
+
+        if ($results !== false) {
+            $data['articlelist'] = $results;
+            return $data;
+        } else {
+            return array('status' => true, 'data' => []);
+        }
+    }
+}
 ?>
