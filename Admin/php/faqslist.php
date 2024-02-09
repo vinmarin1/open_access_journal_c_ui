f6fd20c6d2d89a0b1e9bc474aba8b02fea9d8a26
Arnesky <arnel.mosenabre05@gmail.com>
2024-02-09 19:21:27 +0800

    update

---
+++ Admin/php/faqslist.php
@@ -0,0 +1,341 @@
+<?php
+include 'function/redirect.php';
+include 'function/faqs_function.php';
+
+$faqslist = get_faqs_list();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <!-- Include header -->
+    <?php include 'template/header.php'; ?>
+
+   <!-- Content wrapper -->
+   <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="py-3 mb-4"><span class="text-muted fw-light"></span> FAQS</h4>
+
+        <div class="card">
+        <div style="display: flex; justify-content: space-between; align-items: center; flex-wrap: wrap;">
+            <h5 class="card-header mb-0">FAQS</h5>
+            <div style="display: flex; margin-top: 15px; margin-right: 15px;">
+                <button type="button" id="tabAll" class="btn btn-primary" style="margin-right: 10px;" data-bs-toggle="modal" data-bs-target="#addModal">Add FAQS</button>
+                <!-- <button type="button" id="tabPublished" class="btn btn-primary">Download</button> -->
+            </div>
+        </div>
+
+            <div class="table-responsive text-nowrap">
+                <table class="table table-striped" id="DataTable">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Questions</th>
+                            <th>Answer</th>
+                            <th>Category</th>
+                            <th>Description</th>
+                            <th>Link</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($faqslist as $faqslistval): ?>
+                            <tr>
+                                <td width="5%"><?php echo  $faqslistval->id; ?></td>
+                                <td width="85%"><?php echo  $faqslistval->question; ?></td>
+                                <td width="85%"><?php echo  $faqslistval->answer; ?></td>
+                                <td width="85%"><?php echo  $faqslistval->category; ?></td>
+                                <td width="85%"><?php echo  $faqslistval->description; ?></td>
+                                <td width="85%"><?php echo  $faqslistval->link; ?></td>
+                                <td width="10%">
+                                    <button type="button" class="btn btn-outline-success" onclick="updateModal(<?php echo $faqslistval->id; ?>)">Update</button>
+                                    <button type="button" class="btn btn-outline-danger" onclick="archiveFaqs(<?php echo $faqslistval->id; ?>, '<?php echo $faqslistval->question; ?>', '<?php echo $faqslistval->answer; ?>')">Archive</button>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>    
+                </table>
+            </div>
+        </div>
+
+        <!-- Include footer -->
+        <?php include 'template/footer.php'; ?>
+    </div>
+    
+<!-- Include the DataTables CSS and JS files -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+
+<!-- DataTables initialization script with status filter -->
+<script>
+    $(document).ready(function() {
+        var dataTable = $('#DataTable').DataTable({
+            "paging": true,
+            "ordering": true,
+            "searching": true,
+        });
+    });
+
+    function addRecord() {
+        var form = document.getElementById('addModalForm');
+        var formData = new FormData();
+        formData.append('question', $('#question').val());
+        formData.append('answer', $('#answer').val());
+        formData.append('category', $('#category').val());
+        formData.append('description', $('#description').val());
+        formData.append('link', $('#link').val());
+        formData.append('action', 'add');
+        
+        if (form.checkValidity()) {
+            $('#sloading').toggle();
+            $.ajax({
+                url: "../php/function/faqs_function.php",
+                method: "POST",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (data) {
+                    var response = JSON.parse(data);
+                    if (response.status) {
+                        $('#sloading').toggle();
+                        alert("Record added successfully");
+                    } else {
+                        alert("All fields required!");
+                    }
+                    location.reload();
+                },
+                error: function (xhr, status, error) {
+                    console.error("Ajax request failed:", error);
+                }
+            });
+        } else {
+            form.reportValidity();
+        }
+    }
+
+    function updateModal(id) {
+        $.ajax({
+            type: 'POST',
+            url: '../php/function/faqs_function.php',
+            data: { action: 'fetch', id: id },
+            dataType: 'json',
+            success: function (response) {
+                console.log('Response from server:', response);
+
+                if (response.status === true && response.data.length > 0) {
+                    const faqsData = response.data[0];
+                    console.log('Faqs Data:', faqsData);
+
+                    $('#xid').val(faqsData.id);
+                    $('#xquestion').val(faqsData.question);
+                    $('#xanswer').val(faqsData.answer);
+                    $('#xcategory').val(faqsData.category);
+                    $('#xdescription').val(faqsData.description);
+                    $('#xlink').val(faqsData.link);
+                   
+
+                    $('#updateModal').modal('show');
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('AJAX Error:', textStatus, errorThrown);
+                console.log('Error fetching Faqs data');
+            }
+        });
+    }
+
+    function saveChanges() {
+        $('#sloading').toggle();
+        console.log('Save button clicked');
+        
+        var id = $('#xid').val();
+        var updatedData = {
+            question: $('#xquestion').val(),
+            answer: $('#xanswer').val(),
+            category: $('#xcategory').val(),
+            description: $('#xdescription').val(),
+            link: $('#xlink').val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '../php/function/faqs_function.php',
+            data: {
+                action: 'update',
+                id: id,
+                updated_data: updatedData
+            },
+            dataType: 'json',
+            success: function (response) {
+                console.log('Update Response:', response);
+
+                if (response.status === true) {
+                    $('#sloading').toggle();
+                    alert("Record updated successfully");
+                    $('#updateModal').modal('hide');
+                    location.reload();
+                } else {
+                    console.error('Error updating faqs data:', response.message);
+                    alert("Failed to update record. Please try again.");
+                }
+            },
+        });
+    }
+    
+    function archiveFaqs(id, question, answer) {
+        $('#archiveModal').modal('show');
+        $('#archiveModalTitle').text('Archive FAQS');
+        $('#faqsInfo').html('<strong>Question:</strong> ' + question + ' <br><strong>Answer:</strong> ' + answer + '<br><strong>ID:</strong> ' + id);
+
+        $('#archiveModalSave').off().on('click', function () {
+            $('#sloading').toggle();
+            $.ajax({
+                url: "../php/function/faqs_function.php",
+                method: "POST",
+                data: { action: "archive", id: id },
+                success: function (data) {
+                    var response = JSON.parse(data);
+
+                    if (response.status) {
+                        $('#sloading').toggle();
+                        $('#archiveModalMessage').text('Faqs archived successfully');
+                    } else {
+                        $('#archiveModalMessage').text('Failed to archive faqs');
+                    }
+                        $('#archiveModal').modal('hide');
+                        location.reload();
+                },
+                error: function (xhr, status, error) {
+                    console.error("Ajax request failed:", error);
+                    $('#archiveModalMessage').text('Failed to archive faqs');
+                    $('#archiveModal').modal('hide');
+                    location.reload();
+                }
+            });
+        });
+    }
+</script>
+
+<!-- Add Modal -->
+<div class="modal fade" id="addModal" tabindex="-1" aria-hidden="true">
+    <form id="addModalForm">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel3">Add Faqs</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xquestion" class="form-label">Question</label>
+                            <input type="text" id="question" class="form-control" placeholder="Question" required/>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xanswer" class="form-label">Answer</label>
+                            <input type="text" id="answer" class="form-control" placeholder="Answer" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xcategory" class="form-label">Category</label>
+                            <input type="text" id="category" class="form-control" placeholder="Category" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xdescription" class="form-label">Description</label>
+                            <input type="text" id="description" class="form-control" placeholder="Description" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xlink" class="form-label">Link</label>
+                            <input type="text" id="link" class="form-control" placeholder="Link" />
+                        </div>
+                    </div>  
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="addRecord()">Save changes</button>
+                </div>
+            </div>
+        </div>
+        </form>
+    </div>
+
+         <!-- Update Modal -->
+         <div class="modal fade" id="updateModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel3">Update Faqs</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <input type="hidden" id="xid" class="form-control"/>
+                            <label for="xquestion" class="form-label">Question</label>
+                            <input type="text" id="xquestion" class="form-control" placeholder="Question" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xanswer" class="form-label">Answer</label>
+                            <input type="text" id="xanswer" class="form-control" placeholder="Answer" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xcategory" class="form-label">Category</label>
+                            <input type="text" id="xcategory" class="form-control" placeholder="Category" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xdescription" class="form-label">Description</label>
+                            <input type="text" id="xdescription" class="form-control" placeholder="Description" />
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 mb-2">
+                            <label for="xlink" class="form-label">Link</label>
+                            <input type="text" id="xlink" class="form-control" placeholder="Link" />
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="saveChanges()">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Archive Modal -->
+    <div class="modal fade" id="archiveModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="archiveModalTitle">Archive FAQS</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <h5 class="modal-title" id="modalToggleLabel">Are you sure you want to archive this faqs?</h5>
+                        <p id="faqsInfo"></p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="archiveModalSave">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ Admin/php/function/faqs_function.php
@@ -0,0 +1,118 @@
+<?php
+include 'dbcon.php';
+
+if (!function_exists('get_faqs_list')) {
+    function get_faqs_list()
+    {
+        $pdo = connect_to_database();
+
+        if ($pdo) {
+            try {
+                $query = "SELECT * FROM faqs WHERE status = 1";
+                $stmt = $pdo->query($query);
+
+                $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+                return $result;
+            } catch (PDOException $e) {
+
+                echo "Error: " . $e->getMessage();
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
+
+$action = isset($_POST['action']) ? $_POST['action'] : '';
+
+        switch ($action) {
+            case 'fetch':
+                fetchFaqslData();
+                break;
+            case 'add':
+                addRecord();
+                break;
+            case 'update':
+                updateFaqslData();
+                break;
+            case 'archive':
+                archiveFaqs();
+                break;
+        }
+    
+    function fetchFaqsData() {
+        $id = $_POST['id'];
+    
+        $result = execute_query("SELECT * FROM faqs WHERE id = ?", [$id]);
+    
+        header('Content-Type: application/json');
+    
+        if ($result !== false) {
+            echo json_encode(['status' => true, 'data' => $result]);
+        } else {
+            echo json_encode(['status' => false, 'message' => 'Failed to fetch Faqs data']);
+        }
+    }
+
+    function addRecord() {
+            $question = $_POST['question'];
+            $answer = $_POST['answer'];
+            $category = $_POST['category'];
+            $description = $_POST['description'];
+            $link = $_POST['link'];
+            $status = 1;
+    
+            $query = "INSERT INTO faqs (question, answer, category, description, link, status) 
+        VALUES (?, ?, ?, ?, ?, ?)";
+    
+        $result = execute_query($query, [$question, $answer, $category, $description, $link, $status], true);
+    
+        if ($result !== false) {
+            echo json_encode(['status' => true, 'message' => 'Record added successfully']);
+        } else {
+            echo json_encode(['status' => false, 'message' => 'Failed to add record']);
+        }
+    }
+    
+    function updateFaqsData() {
+        $id = $_POST['id'];
+        $updatedData = $_POST['updated_data'];
+    
+        $query = "UPDATE faqs 
+                    SET question = ?, anwswer = ?, category = ?, description = ?,link = ?
+                    WHERE id = ?";
+        
+        $pdo = connect_to_database();
+    
+        $stm = $pdo->prepare($query);   
+        $check = $stm->execute([
+            $updatedData['question'],
+            $updatedData['answer'],
+            $updatedData['category'],
+            $updatedData['description'],
+            $updatedData['link'],
+            $id
+
+        ]);
+    
+        header('Content-Type: application/json');
+    
+        if ($check !== false) {
+            echo json_encode(['status' => true, 'message' => 'Faqs data updated successfully']);
+        } else {
+            echo json_encode(['status' => false, 'message' => 'Failed to update faqs data']);
+        }
+    }
+
+    function archiveFaqs()
+    {
+        $id = $_POST['id'];
+
+        $query = "UPDATE faqs SET status = 0 WHERE id = ?";
+        $result = execute_query($query, [$id]);
+    
+        echo json_encode(['status' => $result !== false, 'message' => $result !== false ? 'Faqs archived successfully' : 'Failed to archive faqs']);
+    }
+?>
