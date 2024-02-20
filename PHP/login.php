538f7055422dbc9a26ae440430bd49885f4928f4
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-02-20 14:12:21 +0800

    feat: redesign login

--- PHP/login.php
+++ PHP/login.php
@@ -14,41 +14,60 @@
 
 <nav class="navigation-menus-container"  id="navigation-menus-container">
 </nav>
-
-<div class="form-container">
-    <form method="post" id="form">
-        <p class="h4" id="login-label" style="color: black; font-family: Arial, Helvetica, sans-serif">LOGIN</p>
-        <div class="input-field ">
-           <label for="email">Email:</label>
-           <input type="email" class="input form-control" name="email"  id="email" placeholder="Enter your email">
+<div class="d-flex">
+    <div class="form-container" style="background-color: #fafcff   ">
+        <div class="d-flex flex-column gap-4" style="width:75%">
+          <h2 class="font-weight-bold text-xl" style="font-size:40px; font-weight:600;"><span>Quezon City University <span >Journals</span></span> </h2>
+          <span>Enjoy unrestricted access to all articles without logging in! For a tailored experience, exclusive features, and to stay updated with our latest content, log in now.</span>
+          <div>
+            <span class="features">Personalized recommendations</span> 
+            <span class="features">Submit articles</span>
+            <span class="features">Become a reviewer</span>
+            <span class="features">Display your Contributions</span>
+            <span class="features">Download Articles</span>
+          </div>
+          
+          <button class="btn text-white w-50"  style="background-color:#0858a4" onclick="window.location.href='browse-articles.php'">
+            Browse Now
+          </button>
         </div>
-       
-        <div class="input-field">
-           <label for="password">Password:</label>
-           <input type="password" class="input form-control" name="password"  id="password" placeholder="Enter your password">
-           
-        </div>
-        
-        <a id="forgotPasswordLink" style="color: blue; text-decoration: underline; cursor: pointer  "><i class="fas fa-question-circle"></i> Forgot Password?
-            <div class="spinner-border spinner-border-sm" role="status" id="spinner" style="display: none">
-                <span class="visually-hidden"></span>
+    </div>
+    <div class="form-container">
+        <form method="post" id="form">
+            <p class="h4" id="login-label" style="color:rgb(33, 33, 33); font-family: Arial, Helvetica, sans-serif; font-weight:bold">Log in and Access your Account</p>
+            <div class="input-field ">
+               <label for="email">Email:</label>
+               <input type="email" class="input form-control text-xs" name="email"  id="email" placeholder="Enter your email">
             </div>
-        </a>
-        <button type="submit" class="btn btn-primary btn-sm" id="login-button" disabled>
-                <span id="login-text">Login</span>
-                <div class="spinner-border spinner-border-sm" role="status" id="login-spinner" hidden>
-                    <span class="visually-hidden">Loading...</span>
+           
+            <div class="input-field">
+               <label for="password">Password:</label>
+               <input type="password" class="input form-control text-xs" name="password"  id="password" placeholder="Enter your password">
+               
+            </div>
+            
+            <a id="forgotPasswordLink" style="color: #0858a4; text-decoration: underline; cursor: pointer  ">Forgot Password?
+                <div class="spinner-border spinner-border-sm" role="status" id="spinner" style="display: none">
+                    <span class="visually-hidden"></span>
                 </div>
-                <span id="logging-in-text" style="display: none;">Logging in...</span>
-        </button>
-        <span id="countDown" style="color: red"></span>
-        <!-- <input class="btn btn-outline-danger btn-sm" type="button" value="Register" id="register-button" onclick="window.location.href= '../PHP/signup.php';"> -->
-        <button class="btn btn-outline-danger btn-sm" type="button" value="Register" id="register-button" onclick="window.location.href= '../PHP/signup.php';">Register</button>
-    </form>
-    
+            </a>
+            
+            <button type="submit" class="btn btn-primary btn-sm mt-4" id="login-button" disabled>
+                    <span id="login-text">Login</span>
+                    <div class="spinner-border spinner-border-sm" role="status" id="login-spinner" hidden>
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                    <span id="logging-in-text" style="display: none;">Logging in...</span>
+            </button>
+            <span id="countDown" style="color: red"></span>
+            <!-- <input class="btn btn-outline-danger btn-sm" type="button" value="Register" id="register-button" onclick="window.location.href= '../PHP/signup.php';"> -->
+            <button class="btn btn-outline-primary btn-sm" type="button" value="Register" id="register-button" onclick="window.location.href= '../PHP/signup.php';">Register</button>
+            
+        </form>
+        
+    </div>
+
 </div>
-
-
 
 	
 
