f07ff80c6a27350e055d92d9c5be99af0ca444c0
Mon Cedric Avila <moncedricavila@gmail.com>
2024-03-01 22:13:14 +0800

    vendor

--- PHP/success.php
+++ PHP/success.php
@@ -1,5 +1,17 @@
 <?php
 require 'dbcon.php';
+require '../vendor/autoload.php';
+
+$options = array(
+    'cluster' => 'ap1',
+    'useTLS' => true
+);
+$pusher = new Pusher\Pusher(
+    'cabcad916f55a998eaf5',
+    '0aef8b4d2da6760f5726',
+    '1764683',
+    $options
+);
 
 if (!empty($_GET)) {
     session_start();
@@ -11,7 +23,7 @@ if (!empty($_GET)) {
     $_SESSION['status'] = $_GET['st'];
     $_SESSION['payer_id'] = $_GET['payer_id'];
     $_SESSION['payer_email'] = $_GET['payer_email'];
-    $_SESSION['payer_name'] = $_GET['first_name'].' '.$_GET['last_name'];
+    $_SESSION['payer_name'] = $_GET['first_name'] . ' ' . $_GET['last_name'];
 
     $author_id = $_SESSION['product'];
     $product = "Donation";
@@ -20,7 +32,7 @@ if (!empty($_GET)) {
     $payer_name = $_SESSION['payer_name'];
     $payer_firstname = $_GET['first_name'];
     $payer_lastname = $_GET['last_name'];
-    $payer_email = $_SESSION['payer_email']; 
+    $payer_email = $_SESSION['payer_email'];
     $currency = $_SESSION['currency'];
     $amount = $_SESSION['amount'];
     $point_earned = $amount / 50;
@@ -33,13 +45,15 @@ if (!empty($_GET)) {
         $result = database_run($sql, array($author_id, $txn_id, $payer_id, $payer_name, $payer_email, $txn_id, $product, $currency, $amount, $status), true);
 
         if ($result) {
+            $data['message'] = 'hello world';
+            $pusher->trigger('my-channel', 'my-event', $data);
 
             $sql2 = "INSERT INTO `user_points`(`user_id`, `email`, `action_engage`, `point_earned`) VALUES (?, ?, ?, ?)";
 
-            $result2 = database_run($sql2, array($author_id, $payer_email, $product, $point_earned), true); 
+            $result2 = database_run($sql2, array($author_id, $payer_email, $product, $point_earned), true);
 
-            header('Location: donation.php?payer_firstname=' . urlencode($payer_firstname) . '&payer_lastname=' . urlencode($payer_lastname). '&payer_email=' . urlencode($payer_email). '&amount=' . urlencode($amount));
-            exit(); 
+            header('Location: donation.php?payer_firstname=' . urlencode($payer_firstname) . '&payer_lastname=' . urlencode($payer_lastname) . '&payer_email=' . urlencode($payer_email) . '&amount=' . urlencode($amount));
+            exit();
         } else {
             echo "Failed to insert payment information.";
         }
@@ -48,4 +62,3 @@ if (!empty($_GET)) {
         echo "An error occurred while processing the payment.";
     }
 }
-?>
