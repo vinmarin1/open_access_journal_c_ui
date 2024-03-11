5990d7d4c0b22501a5a9ea58f0b8f0fff3dc96a3
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-03-11 22:11:38 +0800

    feat:

--- PHP/footer.php
+++ PHP/footer.php
@@ -4,7 +4,6 @@
 <head>
     <?php include('./meta.php'); ?>
     <title>Footer</title>
-    <!-- <link rel="stylesheet" href="../CSS/index.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
