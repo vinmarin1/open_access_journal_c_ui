3fb378d5deade713b658e3b628ff03fd3fda5b48
Mon Cedric Avila <moncedricavila@gmail.com>
2024-03-24 23:34:22 -0500

    login

--- JS/login.js
+++ JS/login.js
@@ -178,6 +178,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const forgotPasswordLink = document.getElementById('forgotPasswordLink');
         var failedAttempts = 0;
         var disableLoginTimer;
+        var countdownActive = false;
     
         forgotPasswordLink.addEventListener('click', function (event) {
             event.preventDefault();
@@ -233,105 +234,111 @@ document.addEventListener('DOMContentLoaded', function() {
                             $('#login-spinner').hide();
                             $('#login-text').show();
                             startCountdown(advancedAttempt.remainingSeconds);
+                            countdownActive = true;
     
                             function startCountdown(remainingSeconds) {
                                 var countdownInterval = setInterval(function() {
                                     $('#countDown').text('Your Account still disabled. Try again in ' + remainingSeconds + ' seconds');
                                     remainingSeconds--;
-                            
                                     if (remainingSeconds < 0) {
                                         clearInterval(countdownInterval);
                                         $('#countDown').text('');
                                         $('#email').val('');
                                         $('#password').val('');
                                         $('#login-button').prop('disabled', false);
+                                        countdownActive = false;
                                     }
                                 }, 1000);
                             }
                         } else {
-                            $.ajax({
-                                type: "POST",
-                                url: "../PHP/functions.php",
-                                data: {
-                                    email: email,
-                                    password: password,
-                                    urli: urli,
-                                },
-                                success: function(response) {
-                                    var data = JSON.parse(response);
-                                    if (data.success) {
-                                        $('#logging-in-text').text('Logging in...');
-                                        $('#logging-in-text').show();
-                                        $('#login-spinner').show();
-                                        $.ajax({
-                                            type: "POST",
-                                            url: "../PHP/functions.php",
-                                            data: {
-                                                action: "check_verified",
-                                                email: email,
-                                                password: password,
-                                                urli: urli, 
-                                            },
-                                            success: function (verifiedResponse) {
-                                                if (verifiedResponse === "true") {
-                                                    window.location.href = "../PHP/author-dashboard.php";
-                                                } else {
-                                                    window.location.href = "../PHP/verify.php";
-                                                    setTimeout(function() {
-                                                        $('#logging-in-text').hide();
-                                                        $('#login-spinner').hide();
-                                                        $('#login-text').show();
-                                                    }, 5000); 
-                                                }
-                                            },
-                                        });
-                                    } else {
-                                        // Handle failed login attempt
-                                        failedAttempts++;
-                                        if (failedAttempts >= 3) {
+                            if (!countdownActive) { // Check if countdown is not active
+                                $.ajax({
+                                    type: "POST",
+                                    url: "../PHP/functions.php",
+                                    data: {
+                                        email: email,
+                                        password: password,
+                                        urli: urli,
+                                    },
+                                    success: function(response) {
+                                        var data = JSON.parse(response);
+                                        if (data.success) {
+                                            $('#logging-in-text').text('Logging in...');
+                                            $('#logging-in-text').show();
+                                            $('#login-spinner').show();
                                             $.ajax({
                                                 type: "POST",
-                                                url: "attemp_login_email.php",
-                                                data: { email: $('#email').val() }, 
-                                                success: function (response) {
-                                                    console.log("Email sent successfully");
+                                                url: "../PHP/functions.php",
+                                                data: {
+                                                    action: "check_verified",
+                                                    email: email,
+                                                    password: password,
+                                                    urli: urli, 
                                                 },
-                                                error: function (error) {
-                                                    console.error("Error sending email");
-                                                }
+                                                success: function (verifiedResponse) {
+                                                    if (verifiedResponse === "true") {
+                                                        window.location.href = "../PHP/author-dashboard.php";
+                                                    } else {
+                                                        window.location.href = "../PHP/verify.php";
+                                                        setTimeout(function() {
+                                                            $('#logging-in-text').hide();
+                                                            $('#login-spinner').hide();
+                                                            $('#login-text').show();
+                                                        }, 5000); 
+                                                    }
+                                                },
                                             });
-                                            
-                                            var remainingSeconds = 60;
-                                            disableLoginTimer = setInterval(function () {
-                                                var countDownValue = Math.ceil(remainingSeconds);
-                                                $('#countDown').text('Account disabled. Try again in ' + countDownValue + ' seconds');
-                                                $('#email').prop('disabled', true);
-                                                $('#password').prop('disabled', true);
-                                                $('#login-button').prop('disabled', true);
-                                                if (remainingSeconds <= 0) {
-                                                    clearInterval(disableLoginTimer);
-                                                    $('#countDown').text('');
-                                                    $('#email').prop('disabled', false);
-                                                    $('#password').prop('disabled', false);
+                                        } else {
+                                            // Handle failed login attempt
+                                            failedAttempts++;
+                                            if (failedAttempts >= 3) {
+                                                $.ajax({
+                                                    type: "POST",
+                                                    url: "attemp_login_email.php",
+                                                    data: { email: $('#email').val() }, 
+                                                    success: function (response) {
+                                                        console.log("Email sent successfully");
+                                                    },
+                                                    error: function (error) {
+                                                        console.error("Error sending email");
+                                                    }
+                                                });
+                                                
+                                                var remainingSeconds = 60;
+                                                disableLoginTimer = setInterval(function () {
+                                                    var countDownValue = Math.ceil(remainingSeconds);
+                                                    $('#countDown').text('Account disabled. Try again in ' + countDownValue + ' seconds');
+                                                    $('#email').prop('disabled', true);
+                                                    $('#password').prop('disabled', true);
                                                     $('#login-button').prop('disabled', true);
-                                                    failedAttempts = 0; 
-                                                }
-                                                remainingSeconds--;
-                                            }, 1000);
-                                        }
-                                        Swal.fire({
-                                            icon: "error",
-                                            title: "Error",
-                                            text: data.error,
-                                            customClass: {
-                                                container: "custom-swal"
-                                            },
-                                            width: 350,
-                                            height: true,
-                                        });
-                                    }                                    
-                                }
-                            });
+                                                    if (remainingSeconds <= 0) {
+                                                        clearInterval(disableLoginTimer);
+                                                        $('#countDown').text('');
+                                                        $('#email').prop('disabled', false);
+                                                        $('#password').prop('disabled', false);
+                                                        $('#login-button').prop('disabled', true);
+                                                        failedAttempts = 0; 
+                                                    }
+                                                    remainingSeconds--;
+                                                }, 1000);
+                                            }
+                                            Swal.fire({
+                                                icon: "error",
+                                                title: "Error",
+                                                text: data.error,
+                                                customClass: {
+                                                    container: "custom-swal"
+                                                },
+                                                width: 350,
+                                                height: true,
+                                            });
+                                        }                                    
+                                    }
+                                });
+                            } else {
+                                // Countdown is active, do not attempt login
+                                console.log("Countdown is active, login attempt skipped.");
+                            }
                         }
                     }
                 });
