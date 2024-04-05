eabf09fa1ed9502ea51c5972508117f330ac6bb4
MonCedric024 <moncedricavila@gmail.com>
2024-04-05 18:29:54 +0800

    hey notif

--- PHP/header.php
+++ PHP/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <link rel="stylesheet" href="../CSS/header.css">
+  <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
 </head>
 <body>
   <div class="d-none d-sm-flex header">
@@ -24,5 +25,20 @@
         </form>
       </div>
   </div>
+  <script>
+
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = true;
+
+var pusher = new Pusher('cabcad916f55a998eaf5', {
+  cluster: 'ap1'
+});
+
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+  alert(JSON.stringify(data));
+  console.log("Notif");
+});
+</script>
 </body>
 </html>
