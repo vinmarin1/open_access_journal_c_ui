6657b7b7ae96d44f9e79d140f18e5e25c6826924
vinmarin1 <marvin.gozo23@gmail.com>
2024-04-12 16:22:44 +0800

    old db

--- PHP/dbcon.php
+++ PHP/dbcon.php
@@ -1,37 +1,48 @@
 <?php
-// $string = "mysql:host=srv1320.hstgr.io;dbname=u944705315_pahina2024";
-$string = "mysql:host=localhost;dbname=u944705315_pahina2024";
-$username = 'u944705315_pahina2024';
-$password = 'Qcujournal1234.';
 
 function database_run($query, $vars = array(), $isInsert = false)
 {
-    static $con = null; 
-    // Establish connection if not already established
-    if ($con === null) {
-        global $string, $username, $password;
-        try {
-            $con = new PDO($string, $username, $password);
-        } catch (PDOException $e) {
-            return false;
-        }
+    $string = "mysql:host=localhost;dbname=u944705315_pahina2024";
+
+    // $con = new PDO($string, 'u944705315_pahina2024', 'Qcujournal1234.');
+
+    // $string = "mysql:host=localhost;dbname=journal";
+    // $string = "mysql:host=srv1320.hstgr.io;dbname=journal";
+    // $con = new PDO($string, 'root', '');
+
+
+    // if (!$con) {
+    //     return false;
+    // }
+
+    try {
+        $con = new PDO($string, 'u944705315_pahina2024', 'Qcujournal1234.');
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    } catch (PDOException $e) {
+        // If connection fails, redirect to error page
+        header("Location: error.php?message=" . urlencode('Database connection failed: ' . $e->getMessage()));
+        exit();
     }
 
     $stm = $con->prepare($query);
     $check = $stm->execute($vars);
 
     if ($check) {
+      
         if ($isInsert) {
             $lastInsertId = $con->lastInsertId();
+            $con = null; 
             return $lastInsertId;
         }
 
         $data = $stm->fetchAll(PDO::FETCH_OBJ);
+        $con = null;
 
         if (count($data) > 0) {
             return $data;
         }
     }
-
+    $con = null;
     return false;
+    
 }
