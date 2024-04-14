59a3d2dcc30edea08cd26d007789e68f5431fa68
vinmarin1 <marvin.gozo23@gmail.com>
2024-04-14 12:01:22 +0800

    home, tutorials and guidelines tab logo

--- PHP/guidelines.php
+++ PHP/guidelines.php
@@ -13,6 +13,7 @@ $author_id = isset($_SESSION['id']) ? $_SESSION['id'] : 0;
     <link rel="stylesheet" href="../CSS/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="../images/pahina-full.png">
 </head>
 
 <body>
