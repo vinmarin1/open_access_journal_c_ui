16acba9bc9e22e75e68cf00f6a00c144ff51eb0a
vinmarin1 <marvin.gozo23@gmail.com>
2024-04-16 23:50:32 +0800

    qcuj cert logo

--- PHP/user-dashboard.php
+++ PHP/user-dashboard.php
@@ -2074,7 +2074,7 @@ if (isset($_SESSION['LOGGED_IN']) && $_SESSION['LOGGED_IN'] === true) {
 margin-right: auto;">
     <div class="cert-container d-flex justify-content-center align-items-center">
 		<?php 
-			echo '<img class="imgCert" id="cert1" src="../images/cert_review.jpg" alt="cert">'
+			echo '<img class="imgCert" id="cert1" src="../images/qcuj_reviewer_cert.jpg" alt="cert">'
 		?>
         
 		<div class="cert-category">
@@ -2101,7 +2101,7 @@ margin-right: auto;">
 <div class="container-fluid mt-5" id="certPublishedHead" style="display: none; align-items: center !important; width: 800px; height: 500px; margin-left: auto;
 margin-right: auto;">
     <div class="cert-container d-flex justify-content-center align-items-center">
-        <img class="imgCert" id="cert2" src="../images/cert_publish.jpg" alt="cert">
+        <img class="imgCert" id="cert2" src="../images/qcuj_publication_cert.jpg" alt="cert">
 
 			<div class="articlePubInfo">
 				<p class="h2" id="categoryPublished"></p>
