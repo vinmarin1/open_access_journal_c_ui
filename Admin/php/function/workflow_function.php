8c176342a2f728d09390c43402b99e6af6777e27
Mon024 <moncedricavila@gmail.com>
2024-04-26 22:15:45 +0800

    dashboard, reports and workflow

--- Admin/php/function/workflow_function.php
+++ Admin/php/function/workflow_function.php
@@ -437,7 +437,7 @@ if (!function_exists('get_issues_list')) {
 
         if ($pdo) {
             try {
-                $query = "SELECT * FROM issues WHERE journal_id = :journal_id AND status = 1";
+                $query = "SELECT * FROM issues WHERE journal_id = :journal_id AND status = 1 AND year = YEAR(CURDATE())";
                 $stmt = $pdo->prepare($query);
                 $stmt->bindParam(':journal_id', $journal_id, PDO::PARAM_INT);
                 $stmt->execute();
