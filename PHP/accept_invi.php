02577d28f4de1a0b204a36d0e71f3d07fda7c5d2
vinmarin1 <marvin.gozo23@gmail.com>
2024-05-07 22:04:08 +0800

    deadline on accept invitation

--- PHP/accept_invi.php
+++ PHP/accept_invi.php
@@ -10,11 +10,14 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $lastName = $_SESSION['last_name'];
     $middle_name = $_SESSION['middle_name'];
     $userName = $lastName . ' , ' . $firstName .  '  ' . $middle_name;
-    
+
+    date_default_timezone_set('Asia/Manila');
+    $deadlineOneWeekFromNow = date('Y-m-d', strtotime('+1 week'));
     $sqlReject = "UPDATE reviewer_assigned
-                  SET accept = 1
+                  SET accept = 1, deadline = '$deadlineOneWeekFromNow'
                   WHERE article_id = $articleId
-                  AND author_id = $user_id  ORDER BY reviewer_assigned.date_issued DESC
+                  AND author_id = $user_id  
+                  ORDER BY date_issued DESC
                   LIMIT 1";
 
     database_run($sqlReject);
